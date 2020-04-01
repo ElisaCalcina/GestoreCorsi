ebad1884360c0199e321a240d2e0c186d08f4ad0
elisa <elisa@elisa.home-life.hub>
2020-04-01 18:58:56 +0200

    Esercizio completo

--- src/main/java/it/polito/tdp/corsi/db/ConnectDB.java
+++ src/main/java/it/polito/tdp/corsi/db/ConnectDB.java
@@ -1,34 +1,16 @@
 package it.polito.tdp.corsi.db;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 
 public class ConnectDB {
-	private static final String jdbcURL="jdbc:mysql://localhost/iscritticorsi";
-	private static HikariDataSource ds;
 	
-	public static Connection getConnection() {
-		if(ds==null) {
-			HikariConfig config= new HikariConfig();
-			config.setJdbcUrl(jdbcURL);
-			config.setUsername("root");
-			config.setPassword("ac14dg!");
-			
-			config.addDataSourceProperty("cachePrepStmts", true);
-			config.addDataSourceProperty("prepStmtChacheSize", 250);
-			config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
-			
-			ds= new HikariDataSource(config);
-		}
+	public static Connection getConnection() throws SQLException{
+		String jdbcURL="jdbc:mysql://localhost/iscritticorsi?user=root&password=ac14dg!";
+		return DriverManager.getConnection(jdbcURL);
 		
-		try {
-			return ds.getConnection();
-		} catch (SQLException e) {
-			System.err.println("Errore di connnessione ad db");
-			throw new RuntimeException(e);
-		}
 	}
+		
 }
--- src/main/java/it/polito/tdp/corsi/model/Model.java
+++ src/main/java/it/polito/tdp/corsi/model/Model.java
@@ -1,5 +1,6 @@
 package it.polito.tdp.corsi.model;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -12,6 +13,10 @@ public class Model {
 		this.dao= new CorsoDAO();
 	}
 	
+	public boolean esisteCorso(String codins) {
+		return dao.esisteCorso(codins);
+	}
+	
 	public List<Corso> getCorsiByPeriodo(Integer pd){
 		//CorsoDAO dao= new CorsoDAO();
 		return dao.getCorsiByPeriodo(pd);
@@ -20,5 +25,29 @@ public class Model {
 	public Map<Corso, Integer> getIscrittiByPeriodo(Integer pd){
 		//CorsoDAO dao= new CorsoDAO();
 		return dao.getIscrittiByPeriodo(pd);
+	}
+	
+	public List<Studente> getStudentiByCorso(Corso corso){
+		return dao.getStudentiByCorso(corso);
+	}
+	
+	public Map<String, Integer> getDivisioneCDS(Corso corso){
+		//prima soluzione
+		/*List<Studente> studenti= dao.getStudentiByCorso(corso);
+
+		Map<String, Integer> statistiche= new HashMap<String,Integer>(); //hashMap perchè ha chiavi univoche
+		for(Studente s: studenti) {
+			if(s.getCDS()!= null && !s.getCDS().equals("")) {
+			if(statistiche.containsKey(s.getCDS())) {
+				statistiche.put(s.getCDS(), statistiche.get(s.getCDS())+1);
+			}else {
+				statistiche.put(s.getCDS(), 1);
+				}
+			}
+		}
+		return statistiche;*/
+	//Seconda soluzione--> passo dal dao perchè devo implementare la query
+		return dao.getDivisioneCDS(corso);
+		//il controller in questo caso rimane lo stesso
 	}
 }
